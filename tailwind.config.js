b3ecf48c42ed2242be38022969e1851539db83d0
Anshika <anshikaxx107@gmail.com>
2024-12-14 18:10:35 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
-  content:  [
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        "pink" : "#e2a8b6",
-        "lightpink" : "#f4d6d7",
-        "secondary" : "#555",
-        "primary" :"#FCFCFC"
+        "pink": "#e2a8b6",
+        "lightpink": "#f4d6d7",
+        "secondary": "#555",
+        "primary": "#FCFCFC"
       },
-      
     },
   },
   plugins: [
-    require('daisyui'),
+    daisyui, // Updated to use import
   ],
 }
+
 
